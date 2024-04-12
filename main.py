54d6e9e62c65f85c0ebd00905c932d3f4dc62a1e
CUMT-Atom <ccatom@88.com>
2024-04-12 23:17:59 +0800

    🐛bugfix(main):传入bool总为True的问题

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run scripts with specified Python path.')
     parser.add_argument('-env', type=str, nargs='?', default='python', help='The path to the Python interpreter')
     parser.add_argument('-path', type=str, default='data/data.txt', help='The input file path')
-    parser.add_argument('-demo', type=bool, default=False, help='Run the demo')
+    parser.add_argument('-demo', type=str, default='False', help='Run the demo')
     # 解析参数
     args = parser.parse_args()
-    if args.demo:
+    if args.demo == 'True':
         multiprocessing.Process(target=os.system, args=(f'{args.env} utils/gen_random_data.py {args.path}',)).start()
     multiprocessing.Process(target=os.system, args=(f'{args.env} app.py',)).start()
     multiprocessing.Process(target=os.system, args=(f'{args.env} utils/update_data.py {args.path}',)).start()
