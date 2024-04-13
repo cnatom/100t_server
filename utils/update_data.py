28c478ea8e5915d14b1738a98a61fcbf1fb5bdeb
CUMT-Atom <ccatom@88.com>
2024-04-13 15:50:48 +0800

    🐛bugfix(update_data):报警灯的问题

--- utils/update_data.py
+++ utils/update_data.py
@@ -38,7 +38,10 @@ def update_data(input_path='data/data.txt'):
             data_list = last_line.strip().split()[5:]
             for i, data in enumerate(data_list):
                 if i in column_map:
-                    data_dict[column_map[i]] = format(float(data_list[i])/10, '.1f')
+                    if column_map[i] in ['gzbj', 'sn']:
+                        data_dict[column_map[i]] = int(data)
+                    else:
+                        data_dict[column_map[i]] = format(float(data_list[i]) / 10, '.1f')
             # 获取开始时间和当前时间
             if start_time is None and lines:
                 start_time = datetime.strptime(lines[0].split()[0], '%H:%M:%S')
